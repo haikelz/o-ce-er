742fe14420cf81c59cbd60bce26c49d78e3c6f37
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2025-08-03 02:33:38 +0700

    feat: finalize read image to text logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,70 @@
 import { useState } from "react";
-import Markdown from "react-markdown";
+import Dropzone from "react-dropzone";
+import ShikiHighlighter from "react-shiki";
 import { createWorker } from "tesseract.js";
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
 
 function App() {
   const [result, setResult] = useState("");
+  const [file, setFile] = useState<File | null>(null);
 
-  async function readImageToText(imagePath: string) {
+  async function convertImageToText() {
     const worker = await createWorker("eng");
-    const ret = await worker.recognize(imagePath);
+    const ret = await worker.recognize(file as File);
 
-    console.log(ret.data);
     setResult(ret.data.text);
     await worker.terminate();
   }
 
+  function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const file = event.target.files?.[0];
+    if (file) {
+      setFile(file);
+    }
+  }
+
+  function handleDrop(acceptedFiles: File[]) {
+    setFile(acceptedFiles[0]);
+    convertImageToText();
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <Input type="file" />
-      <Button onClick={() => readImageToText("/struk-example.png")}>
-        Convert
-      </Button>
-      <p>OCR Services</p>
-      <p>Please input the picture that you want to convert to text</p>
-      <p className="prose prose-sm prose-sky">
-        {result ? <Markdown>{result}</Markdown> : "Belum ada hasil!"}
-      </p>
-    </div>
+    <main className="flex flex-col prose prose-sm prose-sky max-w-full w-full items-center justify-center min-h-screen">
+      <div className="flex p-4 flex-col items-center justify-center max-w-7xl">
+        <div className="text-center">
+          <h1 className="text-2xl font-bold">O-Ce-Er Services by ekel.dev</h1>
+          <p className="text-sm text-gray-500">
+            O-Ce-Er is a OCR services that can convert image to text and also
+            convert text to image.
+          </p>
+        </div>
+        <Dropzone onDrop={handleDrop}>
+          {({ getRootProps }) => (
+            <div className="flex items-center justify-center gap-2">
+              <Input
+                type="file"
+                onChange={handleFileChange}
+                accept="image/*"
+                {...getRootProps()}
+              />
+              <Button type="submit" onClick={convertImageToText}>
+                Convert
+              </Button>
+            </div>
+          )}
+        </Dropzone>
+        <div className="mt-6 w-full">
+          {result ? (
+            <ShikiHighlighter language="text" theme="github-dark">
+              {result.trim()}
+            </ShikiHighlighter>
+          ) : (
+            <p className="text-center">No result!</p>
+          )}
+        </div>
+      </div>
+    </main>
   );
 }
 
