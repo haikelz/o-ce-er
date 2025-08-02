fc8914b5981afcea8415c0b8b7783e3d9867ac0c
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2025-08-03 06:14:41 +0700

    feat(ocr): implement cancel image, convert button placement, and show image filename

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,122 @@
+import { ImageIcon, X } from "lucide-react";
 import { useState } from "react";
-import Dropzone from "react-dropzone";
+import Dropzone, { useDropzone } from "react-dropzone";
 import ShikiHighlighter from "react-shiki";
 import { createWorker } from "tesseract.js";
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
+import { cn } from "./lib/utils";
 
 function App() {
-  const [result, setResult] = useState("");
+  const [result, setResult] = useState<string | null>(null);
   const [file, setFile] = useState<File | null>(null);
+  const [isConverting, setIsConverting] = useState(false);
 
-  async function convertImageToText() {
+  const { getRootProps, isDragActive } = useDropzone({
+    onDrop: (acceptedFiles) => {
+      setFile(acceptedFiles[0]);
+    },
+  });
+
+  async function convertImageToText(): Promise<void> {
     const worker = await createWorker("eng");
+    setIsConverting(true);
+
     const ret = await worker.recognize(file as File);
 
+    setIsConverting(false);
     setResult(ret.data.text);
     await worker.terminate();
   }
 
-  function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
-    const file = event.target.files?.[0];
+  function handleFileChange(e: React.ChangeEvent<HTMLInputElement>): void {
+    const file = e.target.files?.[0];
+
     if (file) {
       setFile(file);
     }
   }
 
-  function handleDrop(acceptedFiles: File[]) {
-    setFile(acceptedFiles[0]);
-    convertImageToText();
-  }
-
   return (
     <main className="flex flex-col prose prose-sm prose-sky max-w-full w-full items-center justify-center min-h-screen">
-      <div className="flex p-4 flex-col items-center justify-center max-w-7xl">
+      <section className="flex p-4 flex-col items-center justify-center max-w-7xl">
         <div className="text-center">
-          <h1 className="text-2xl font-bold">O-Ce-Er Services by ekel.dev</h1>
+          <h1 className="text-3xl font-bold">O-Ce-Er</h1>
           <p className="text-sm text-gray-500">
-            O-Ce-Er is a OCR services that can convert image to text and also
-            convert text to image.
+            O-Ce-Er is a serverless OCR services that can convert image to text.
+            Brought to you by{" "}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://ekel.dev"
+              className="text-blue-500"
+            >
+              ekel.dev
+            </a>
           </p>
         </div>
-        <Dropzone onDrop={handleDrop}>
-          {({ getRootProps }) => (
-            <div className="flex items-center justify-center gap-2">
-              <Input
-                type="file"
-                onChange={handleFileChange}
-                accept="image/*"
-                {...getRootProps()}
-              />
-              <Button type="submit" onClick={convertImageToText}>
-                Convert
-              </Button>
-            </div>
-          )}
-        </Dropzone>
+        <div className="flex flex-col items-center justify-center text-center">
+          <Dropzone onDrop={getRootProps}>
+            {({ getRootProps }) => (
+              <div
+                className={cn(
+                  isDragActive ? "p-3" : "",
+                  "flex decoration-2 border border-dashed rounded-md overflow-hidden relative w-64 h-64 items-center justify-center gap-2"
+                )}
+              >
+                {file ? (
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className="absolute top-0 left-0 z-10"
+                    onClick={() => setFile(null)}
+                  >
+                    <X className="w-4 h-4" />
+                  </Button>
+                ) : null}
+                <Input
+                  type="file"
+                  onChange={handleFileChange}
+                  accept="image/*"
+                  {...getRootProps()}
+                  className="opacity-0 w-full h-full absolute"
+                />
+                {file ? (
+                  <img
+                    src={URL.createObjectURL(file)}
+                    alt={file.name || "image"}
+                    className="w-full h-full object-cover"
+                  />
+                ) : (
+                  <div className="flex flex-col items-center justify-center gap-2">
+                    <ImageIcon className="w-10 h-10 text-gray-500" />
+                    <p className="text-sm text-gray-500">
+                      Select image here or drag and drop
+                    </p>
+                  </div>
+                )}
+              </div>
+            )}
+          </Dropzone>
+          <p>{file ? file.name : "No file selected"}</p>
+          <Button
+            onClick={convertImageToText}
+            className="w-full"
+            disabled={isConverting || !file}
+          >
+            {isConverting ? "Converting..." : "Convert"}
+          </Button>
+        </div>
         <div className="mt-6 w-full">
           {result ? (
             <ShikiHighlighter language="text" theme="github-dark">
               {result.trim()}
             </ShikiHighlighter>
-          ) : (
+          ) : result !== null ? (
             <p className="text-center">No result!</p>
-          )}
+          ) : null}
         </div>
-      </div>
+      </section>
     </main>
   );
 }
